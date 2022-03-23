d062f6b851277d9f3a3a76467bb6bb1022e417ec
Bryn Deering <bdeering@uoguelph.ca>
2022-03-22 23:56:24 -0400

    Removed unnecessary enums and other clean up

--- source/nhl_stats.py
+++ source/nhl_stats.py
@@ -12,77 +12,87 @@ FRANCHISES_ROUTE = '/api/v1/franchises'
 STANDINGS_ROUTE =  '/api/v1/standings'
 STATS_TYPES_ROUTE = '/api/v1/statTypes'
 
-class StatsBy(Enum):
-  SEASON = 'statsSingleSeason'
-  PACE = 'onPaceRegularSeason'
-  YEAR = 'yearByYear'
-  HOME_AWAY = 'homeAndAway'
-  WIN_LOSS = 'winLoss'
-  MONTH = 'byMonth'
-  DAY_OF_WEEK = 'byDayOfWeek'
-  GAME = 'gameLog'
-class StatsVs(Enum):
-  DIVISION = 'vsDivision'
-  CONFERENCE = 'vsConference'
-class StatsMisc(Enum):
-  GOALS_BY_SITUATION = 'goalsByGameSituation'
-  SEASON_RANKINGS = 'regularSeasonStatRankings'
+""" STAT TYPES
+yearByYear
+yearByYearRank
+yearByYearPlayoffs
+yearByYearPlayoffsRank
+careerRegularSeason
+careerPlayoffs
+gameLog
+playoffGameLog
+vsTeam
+vsTeamPlayoffs
+vsDivision
+vsDivisionPlayoffs
+vsConference
+vsConferencePlayoffs
+byMonth
+byMonthPlayoffs
+byDayOfWeek
+byDayOfWeekPlayoffs
+homeAndAway
+homeAndAwayPlayoffs
+winLoss
+winLossPlayoffs
+onPaceRegularSeason
+regularSeasonStatRankings
+playoffStatRankings
+goalsByGameSituation
+goalsByGameSituationPlayoffs
+statsSingleSeason
+statsSingleSeasonPlayoffs
+"""
+
 
 def main(argv):
   print('\n\nRetrieving data...')
-  print_team_stats()
 
 
 #Output
 def print_team_stats(num_teams = 1):
-  for t_route, t_name in get_team_routes(num_teams):
-    print(t_name)
-    pprint(get_team_stats(t_route))
+  for route in get_team_routes(num_teams):
+    print(get_team_name(route))
+    pprint(get_team_stats(route))
 
-def print_player_stats(num_teams = None, num_players = None):
+def print_player_stats(num_teams = 1, num_players = None):
   for route in get_player_routes(num_teams, num_players):
-    print(get_json(route)['people'][0]['fullName'])
-    stat_params = create_stats_params(StatsBy.SEASON)
-    pprint(get_player_stats(route, stat_params))
+    print(get_player_name(route))
+    pprint(get_player_stats(route, create_stats_params('statsSingleSeason')))
 
-def print_players(num_teams = 1, num_players = None):
-  count = 0
-  for t, t_route in enumerate(get_team_routes(num_teams)):
-    print('\n' + get_json(t_route)['teams'][0]['name'])
-    for p, p_route in enumerate(get_roster_routes(t_route, num_players)):
-      player_data = get_json(p_route)['people'][0]
-      print(f"{player_data['fullName']:24}{player_data['primaryPosition']['type']}")
-    count += p + 1
-  print('\n' + str(count) + ' players')
-  printlb(t + 1, 'teams')
+def get_team_name(route):
+  return get_json(route)['teams'][0]['name']
+
+def get_player_name(route):
+  return get_json(route)['people'][0]['fullName']
 
 
 #Stats
+def get_team_stats(team_route) -> dict:
+  return get_json(team_route + '/stats')['stats']
+  
 def get_player_stats(player_route, params) -> dict:
   return get_json(player_route + '/stats' + params)['stats'][0]['splits']
 
-def get_team_stats(team_route):
-  return get_json(team_route + '/stats')['stats']
-
-def create_stats_params(stat_option: Enum, season: str = '20202021'):
-  params = '?stats=' + stat_option.value
-  if stat_option != StatsBy.YEAR:
+def create_stats_params(stat_option: str, season: str = '20202021') -> str:
+  params = '?stats=' + stat_option
+  if 'yearByYear' not in stat_option:
     params += '&season=' + season
   return params
 
 
 # Routes
-def get_player_routes(num_teams = 1, num_players = None) -> types.GeneratorType:
-  for team_route in get_team_routes(num_teams):
-    yield from get_roster_routes(team_route, num_players)
+def get_player_routes(num_teams = None, num_players = None) -> types.GeneratorType:
+  for team in get_team_routes(num_teams):
+    yield from get_roster_routes(team, num_players)
+
+def get_team_routes(qty = None) -> types.GeneratorType:
+  for team in get_json(TEAMS_ROUTE)['teams'][:qty]:
+    yield team['link']
 
 def get_roster_routes(team_route, qty = None) -> types.GeneratorType:
   for player in get_json(team_route + '/roster')['roster'][:qty]:
     yield player['person']['link']
-
-def get_team_routes(qty = None) -> types.GeneratorType: # not sure about this implementation
-  for team in get_json(TEAMS_ROUTE)['teams'][:qty]:
-    yield team['link'], team['name']
 
 
 def get_json(route: str) -> dict:
